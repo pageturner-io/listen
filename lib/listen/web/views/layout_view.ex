d3c7cc1e5528d266416dd869b939321b84898e85
Bruno Abrantes <bruno@brunoabrantes.com>
2017-03-18 00:28:47 +0100

    Finishes basic page design

--- lib/listen/web/views/layout_view.ex
+++ lib/listen/web/views/layout_view.ex
@@ -1,12 +1,12 @@
 defmodule Listen.Web.LayoutView do
   use Listen.Web, :view
 
-  def identity_path(conn, :login) do
+  def identity_path(_conn, :login) do
     Application.fetch_env!(:listen, :identity_base_path)
     |> Path.join("login")
   end
 
-  def identity_path(conn, :register) do
+  def identity_path(_conn, :register) do
     Application.fetch_env!(:listen, :identity_base_path)
     |> Path.join("register")
   end
