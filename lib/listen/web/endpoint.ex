31555684a1795392ea38671607a06e1bc947ac6c
Bruno Abrantes <bruno@brunoabrantes.com>
2017-04-05 00:38:02 +0200

    Makes session cookie name more descriptive

--- lib/listen/web/endpoint.ex
+++ lib/listen/web/endpoint.ex
@@ -35,7 +35,7 @@ defmodule Listen.Web.Endpoint do
   # Set :encryption_salt if you would also like to encrypt it.
   plug Plug.Session,
     store: :cookie,
-    key: "_web_key",
+    key: "_listen_key",
     signing_salt: "1BL1NQvh"
 
   plug Listen.Web.Router
